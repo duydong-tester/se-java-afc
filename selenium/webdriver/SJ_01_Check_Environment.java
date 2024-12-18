c2f7eddad1b2582154f45525c95db6f208d70c0e
Do Duy Dong <doduydong.97d@gmail.com>
2024-12-18 12:34:19 +0700

    Update Selenium, TestNG libraries

--- selenium/webdriver/SJ_01_Check_Environment.java
+++ selenium/webdriver/SJ_01_Check_Environment.java
@@ -1,45 +1,32 @@
 package webdriver;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class SJ_01_Check_Environment {
     WebDriver driver;
-    String projectPath = System.getProperty("user.dir");
 
-    @BeforeClass
-    public void beforeClass() {
-        System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+    @Test
+    public void TC_01_Firefox() {
         driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
         driver.get("https://www.facebook.com/");
+        driver.quit();
     }
 
     @Test
-    public void TC_01_Url() {
-        Assert.assertEquals(driver.getCurrentUrl(), "https://www.facebook.com/");
+    public void TC_02_Chrome() {
+        driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
     }
 
     @Test
-    public void TC_02_Logo() {
-        Assert.assertTrue(driver.findElement(By.cssSelector("img.fb_logo")).isDisplayed());
-    }
-
-    @Test
-    public void TC_03_Form() {
-        Assert.assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
-    }
-
-    @AfterClass
-    public void afterClass() {
+    public void TC_03_Edge() {
+        driver = new EdgeDriver();
+        driver.get("https://www.facebook.com/");
         driver.quit();
     }
 
